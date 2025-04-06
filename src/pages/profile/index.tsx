514da55caa6d4b21c2835d742446eadfce7b104f
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-04-06 22:42:05 +0330

    fix: handle close/open hamburger menu in mobile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,12 +1,18 @@
 import { Divider } from '@mui/material';
+import { useDispatch } from 'react-redux';
 import { translate } from 'src/core/helpers/utils';
+import BackLink from 'src/modules/General/components/BackLink';
 import ManageProfile from 'src/modules/Profile/ManageProfile';
+import { showMenu } from 'src/store/reducers/menu.reducer';
 
 import styles from './index.module.scss';
 
 export const Profile = () => {
+  const dispatch = useDispatch();
+
   return (
     <div className={styles['container']}>
+      <BackLink title="Back" customStyle={styles['back']} onBack={() => dispatch(showMenu())} />
       <div className={styles['header']}>
         <h1 className={styles['header__title']}>{translate('profile-title')}</h1>
         <h2 className={styles['header__subtitle']}>{translate('profile-subtitle')}</h2>
