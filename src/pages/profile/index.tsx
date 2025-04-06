2206a4a38a9482d3455091916fd1b569d429eefa
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-04-06 19:25:30 +0330

    feat: account profile page and management

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,7 +1,18 @@
+import { Divider } from '@mui/material';
+import { translate } from 'src/core/helpers/utils';
+import ManageProfile from 'src/modules/Profile/ManageProfile';
+
+import styles from './index.module.scss';
+
 export const Profile = () => {
   return (
-    <div>
-      <span>Account profile</span>
+    <div className={styles['container']}>
+      <div className={styles['header']}>
+        <h1 className={styles['header__title']}>{translate('profile-title')}</h1>
+        <h2 className={styles['header__subtitle']}>{translate('profile-subtitle')}</h2>
+      </div>
+      <Divider />
+      <ManageProfile />
     </div>
   );
 };
