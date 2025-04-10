a3ee523bac9357bf1ea35dbe1844c7e72e9a538b
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-04-10 20:06:17 +0330

    feat: alert message as a general component

---
+++ src/modules/General/components/AlertMessage/index.tsx
@@ -0,0 +1,32 @@
+import { convertMarkdownToJSX } from 'src/core/helpers/convert-md-to-jsx';
+
+import styles from './index.module.scss';
+import { AlertMessageProps } from './index.types';
+import FeaturedIconOutlined from '../FeaturedIconOutlined';
+
+const AlertMessage: React.FC<AlertMessageProps> = ({
+  theme,
+  iconName,
+  title,
+  subtitle = '',
+  children,
+  colOrderMobileView,
+  containerClassName = '',
+}) => {
+  return (
+    <div
+      className={`${styles['container']} ${styles[`container--${theme}`]} ${colOrderMobileView && 'flex-col md:flex-row'} ${containerClassName}`}
+    >
+      <FeaturedIconOutlined iconName={iconName} size="md" theme={theme} />
+      <div className={styles['header']}>
+        <span className={`${styles['title']} ${styles[`title--${theme}`]}`}>{title}</span>
+        {subtitle && (
+          <span className={`${styles['subtitle']} ${styles[`title--${theme}`]}`}>{convertMarkdownToJSX(subtitle)}</span>
+        )}
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default AlertMessage;
