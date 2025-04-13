42886defa59a6edaca4217f6ef3814928e83f657
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-04-13 17:40:22 +0330

    feat: add icon theme for alert message component

--- src/modules/General/components/AlertMessage/index.types.ts
+++ src/modules/General/components/AlertMessage/index.types.ts
@@ -3,6 +3,7 @@ import { ReactNode } from 'react';
 export interface AlertMessageProps {
   theme: 'primary' | 'gray' | 'error' | 'warning' | 'success';
   iconName: string;
+  iconTheme?: 'primary' | 'gray' | 'error' | 'warning' | 'success';
   title: string;
   subtitle?: string;
   children?: ReactNode;
