6dde6481f2e2dc80f6adedb2e38d55817f5baaa6
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-05-07 17:50:25 +0330

    feat: translations for settings page and hamburger menu

--- src/core/translation/locales/en/translation.ts
+++ src/core/translation/locales/en/translation.ts
@@ -3,8 +3,9 @@ import impact from './impact.json';
 import layout from './layout.json';
 import password from './password.json';
 import profile from './profile.json';
+import settings from './settings.json';
 import verification from './verification.json';
 
 export function generateTranslationFile() {
-  return Object.assign(general, layout, verification, profile, password, impact, {});
+  return Object.assign(general, layout, verification, profile, password, impact, settings, {});
 }
