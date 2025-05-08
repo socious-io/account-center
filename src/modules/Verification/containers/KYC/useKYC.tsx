e41551b85a2df7484d0c1a96303e3c3d0a902ba3
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-05-08 14:25:07 +0330

    feat: exceeded verification alert box

--- src/modules/Verification/containers/KYC/useKYC.tsx
+++ src/modules/Verification/containers/KYC/useKYC.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import { isMobile } from 'react-device-detect';
+import { useNavigate } from 'react-router-dom';
+import { createVerificationAdaptor } from 'src/core/adaptors';
 
 export const useKYC = (connectUrl: string) => {
+  const navigate = useNavigate();
   const [openQRModal, setOpenQRModal] = useState(false);
 
   const onVerify = () => {
@@ -13,6 +16,12 @@ export const useKYC = (connectUrl: string) => {
     }
   };
 
+  const onCreateVerification = async () => {
+    const { error, data } = await createVerificationAdaptor();
+    if (error) return;
+    if (data) navigate('/verification');
+  };
+
   return {
     data: {
       openQRModal,
@@ -20,6 +29,7 @@ export const useKYC = (connectUrl: string) => {
     operations: {
       setOpenQRModal,
       onVerify,
+      onCreateVerification,
     },
   };
 };
