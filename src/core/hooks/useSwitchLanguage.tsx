a518f2409c6dc9faff85021d71e4eca3f70c369b
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-05-08 17:34:18 +0330

    feat: language on query param

--- src/core/hooks/useSwitchLanguage.tsx
+++ src/core/hooks/useSwitchLanguage.tsx
@@ -1,17 +1,21 @@
 import i18next from 'i18next';
 import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { LANGUAGES } from 'src/constants/LANGUAGES';
 
 const useSwitchLanguage = (defaultLanguage = 'en') => {
-  const [selectedLanguage, setSelectedLanguage] = useState(() => {
-    return localStorage.getItem('language') || defaultLanguage;
-  });
+  const [selectedLanguage, setSelectedLanguage] = useState(localStorage.getItem('i18nextLng') || defaultLanguage);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lang = searchParams.get('lang');
 
   const switchLanguage = language => {
     if (LANGUAGES.map(lang => lang.value).includes(language)) {
       setSelectedLanguage(language);
       i18next.changeLanguage(language);
-      localStorage.setItem('language', language);
+      if (lang) {
+        searchParams.set('lang', language);
+        setSearchParams(searchParams);
+      }
       window.location.reload();
     } else {
       console.warn(`Language '${language}' is not supported.`);
