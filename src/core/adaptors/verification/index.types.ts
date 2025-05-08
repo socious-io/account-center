413976375f40f0e4b47f0949b79e7ccca46d9c2d
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-05-08 14:23:15 +0330

    feat: APIs/adaptors/types for exceeded error on verification

--- src/core/adaptors/verification/index.types.ts
+++ src/core/adaptors/verification/index.types.ts
@@ -1,9 +1,10 @@
 import { UserVerificationStatus } from 'src/core/api';
 
-export type KYCStatus = '' | 'succeed' | 'failed';
+export type KYCStatus = '' | 'succeed' | 'failed' | 'exceeded';
 
 export interface KYC {
   id: string;
   connectURL: string;
   status: UserVerificationStatus;
+  validationError?: string;
 }
