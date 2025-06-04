a35bd2971157d202c21784a8a655ad6c9d7e4986
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-06-04 14:58:40 +0330

    feat: API/adaptor/types for refer page

---
+++ src/core/adaptors/refer/index.types.ts
@@ -0,0 +1,31 @@
+import { PaginateRes } from '..';
+
+export type ReferAchievement = {
+  //FIXME: add some fixed types according to BE
+  type: string;
+  total: number;
+};
+
+export interface ReferOverviews {
+  achievements: ReferAchievement[];
+  totalRewards: number;
+  unclaimedRewards: number;
+  totalReferrals: number;
+}
+
+export type ClaimedAchievement = {
+  claimed: boolean;
+  did?: boolean;
+};
+
+export interface Referral {
+  identity: {
+    name: string;
+    username: string;
+    img?: string;
+  };
+  date: Date;
+  [achievement: string]: ClaimedAchievement | any;
+}
+
+export type MyReferralRes = PaginateRes<Referral>;
