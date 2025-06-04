c5766245013ab0396038f012c17026ef6f7e0f6c
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-06-04 15:18:00 +0330

    feat: translations for refer page

--- src/core/translation/locales/kr/translation.ts
+++ src/core/translation/locales/kr/translation.ts
@@ -4,9 +4,10 @@ import layout from './layout.json';
 import password from './password.json';
 import payments from './payments.json';
 import profile from './profile.json';
+import refer from './refer.json';
 import settings from './settings.json';
 import verification from './verification.json';
 
 export function generateTranslationFile() {
-  return Object.assign(general, layout, verification, profile, password, impact, settings, payments, {});
+  return Object.assign(general, layout, verification, profile, password, impact, settings, payments, refer, {});
 }
